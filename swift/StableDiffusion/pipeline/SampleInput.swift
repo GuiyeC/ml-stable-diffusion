47364a0c15f2a5f64da0df4b0f60670bd6694fe9
Guillermo Cique Fernández <GuiyeC@gmail.com>
2022-12-13 23:16:31 +0100

    Add negativePrompt and inpaintMask to SampleInput

--- swift/StableDiffusion/pipeline/SampleInput.swift
+++ swift/StableDiffusion/pipeline/SampleInput.swift
@@ -10,8 +10,10 @@ import CoreGraphics
 
 public struct SampleInput: Hashable {
     public var prompt: String
+    public var negativePrompt: String
     public var initImage: CGImage?
     public var strength: Float?
+    public var inpaintMask: CGImage?
     public var seed: Int
     public var stepCount: Int
     /// Controls the influence of the text prompt on sampling process (0=random images)
@@ -19,13 +21,16 @@ public struct SampleInput: Hashable {
     
     public init(
         prompt: String,
+        negativePrompt: String = "",
         seed: Int = Int.random(in: 0...Int(UInt32.max)),
         stepCount: Int = 20,
         guidanceScale: Float = 7.5
     ) {
         self.prompt = prompt
+        self.negativePrompt = negativePrompt
         self.initImage = nil
         self.strength = nil
+        self.inpaintMask = nil
         self.seed = seed
         self.stepCount = stepCount
         self.guidanceScale = guidanceScale
@@ -33,15 +38,37 @@ public struct SampleInput: Hashable {
     
     public init(
         prompt: String,
+        negativePrompt: String = "",
         initImage: CGImage?,
         strength: Float = 0.75,
         seed: Int = Int.random(in: 0...Int(UInt32.max)),
         stepCount: Int = 20,
-        guidanceScale: Float = 5.0
+        guidanceScale: Float = 7.5
     ) {
         self.prompt = prompt
+        self.negativePrompt = negativePrompt
         self.initImage = initImage
         self.strength = strength
+        self.inpaintMask = nil
+        self.seed = seed
+        self.stepCount = stepCount
+        self.guidanceScale = guidanceScale
+    }
+    
+    public init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: CGImage?,
+        inpaintMask: CGImage,
+        seed: Int = Int.random(in: 0...Int(UInt32.max)),
+        stepCount: Int = 20,
+        guidanceScale: Float = 7.5
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        self.initImage = initImage
+        self.strength = nil
+        self.inpaintMask = inpaintMask
         self.seed = seed
         self.stepCount = stepCount
         self.guidanceScale = guidanceScale
@@ -54,15 +81,56 @@ import UIKit
 public extension SampleInput {
     init(
         prompt: String,
+        negativePrompt: String = "",
         initImage: UIImage,
         strength: Float = 0.75,
         seed: Int = Int.random(in: 0...Int(UInt32.max)),
         stepCount: Int = 50,
-        guidanceScale: Float = 5.0
+        guidanceScale: Float = 7.5
     ) {
         self.prompt = prompt
+        self.negativePrompt = negativePrompt
         self.initImage = initImage.cgImage!
         self.strength = strength
+        self.inpaintMask = nil
+        self.seed = seed
+        self.stepCount = stepCount
+        self.guidanceScale = guidanceScale
+    }
+    
+    init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: UIImage,
+        inpaintMask: CGImage,
+        seed: Int = Int.random(in: 0...Int(UInt32.max)),
+        stepCount: Int = 50,
+        guidanceScale: Float = 7.5
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        self.initImage = initImage.cgImage!
+        self.strength = nil
+        self.inpaintMask = inpaintMask
+        self.seed = seed
+        self.stepCount = stepCount
+        self.guidanceScale = guidanceScale
+    }
+    
+    init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: UIImage,
+        inpaintMask: UIImage,
+        seed: Int = Int.random(in: 0...Int(UInt32.max)),
+        stepCount: Int = 50,
+        guidanceScale: Float = 7.5
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        self.initImage = initImage.cgImage!
+        self.strength = nil
+        self.inpaintMask = inpaintMask.cgImage!
         self.seed = seed
         self.stepCount = stepCount
         self.guidanceScale = guidanceScale
@@ -76,16 +144,60 @@ import AppKit
 public extension SampleInput {
     init(
         prompt: String,
+        negativePrompt: String = "",
         initImage: NSImage,
         strength: Float = 0.75,
         seed: Int = Int.random(in: 0...Int(UInt32.max)),
         stepCount: Int = 50,
-        guidanceScale: Float = 5.0
+        guidanceScale: Float = 7.5
     ) {
         self.prompt = prompt
+        self.negativePrompt = negativePrompt
         var imageRect = CGRect(x: 0, y: 0, width: initImage.size.width, height: initImage.size.height)
         self.initImage = initImage.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)!
         self.strength = strength
+        self.inpaintMask = nil
+        self.seed = seed
+        self.stepCount = stepCount
+        self.guidanceScale = guidanceScale
+    }
+    
+    init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: NSImage,
+        inpaintMask: CGImage,
+        seed: Int = Int.random(in: 0...Int(UInt32.max)),
+        stepCount: Int = 50,
+        guidanceScale: Float = 7.5
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        var imageRect = CGRect(x: 0, y: 0, width: initImage.size.width, height: initImage.size.height)
+        self.initImage = initImage.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)!
+        self.strength = nil
+        self.inpaintMask = inpaintMask
+        self.seed = seed
+        self.stepCount = stepCount
+        self.guidanceScale = guidanceScale
+    }
+    
+    init(
+        prompt: String,
+        negativePrompt: String = "",
+        initImage: NSImage,
+        inpaintMask: NSImage,
+        seed: Int = Int.random(in: 0...Int(UInt32.max)),
+        stepCount: Int = 50,
+        guidanceScale: Float = 7.5
+    ) {
+        self.prompt = prompt
+        self.negativePrompt = negativePrompt
+        var imageRect = CGRect(x: 0, y: 0, width: initImage.size.width, height: initImage.size.height)
+        self.initImage = initImage.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)!
+        self.strength = nil
+        var inpaintMaskRect = CGRect(x: 0, y: 0, width: inpaintMask.size.width, height: inpaintMask.size.height)
+        self.inpaintMask = inpaintMask.cgImage(forProposedRect: &inpaintMaskRect, context: nil, hints: nil)!
         self.seed = seed
         self.stepCount = stepCount
         self.guidanceScale = guidanceScale
