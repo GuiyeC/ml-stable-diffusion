0c09c3e403bd06244f9e616b2dccb2704b5cd87a
Guillermo Cique Fernández <GuiyeC@gmail.com>
2023-01-11 02:30:02 +0100

    Package converter as a DMG instead of PKG

--- GuernikaModelConverter/build.sh
+++ GuernikaModelConverter/build.sh
@@ -2,9 +2,12 @@
 
 pyinstaller GuernikaModelConverter/GuernikaModelConverter.spec --clean -y
 codesign -s 'F3CA2B3DA0C3FA37A8F800B0EDCEF3923C36E753' -v --deep --force --options=runtime --timestamp --entitlements GuernikaModelConverter/GuernikaModelConverter.entitlements -o runtime "dist/Guernika Model Converter.app"
-ditto "dist/Guernika Model Converter.app" "dist/pkg/Applications/Guernika Model Converter.app"
-productbuild --identifier "com.guiyec.GuernikaModelConverter" --sign "01F6504F86D22EB924A51B2DFA28E5483A3172B6" --timestamp --root dist/pkg / dist/GuernikaModelConverter.pkg
-rm -rf dist/pkg
+ditto "dist/Guernika Model Converter.app" "dist/dmg/Guernika Model Converter.app"
+hdiutil create dist/tmp_guernikaconverter.dmg -ov -volname "GuernikaModelConverter" -fs APFS -srcfolder "dist/dmg/"
+hdiutil convert dist/tmp_guernikaconverter.dmg -format UDZO -o dist/GuernikaModelConverter.dmg
+codesign -s 'F3CA2B3DA0C3FA37A8F800B0EDCEF3923C36E753' --timestamp dist/GuernikaModelConverter.dmg
+rm -rf dist/dmg
+rm dist/tmp_guernikaconverter.dmg
 #xcrun notarytool store-credentials --apple-id "guiyec@gmail.com" --team-id "A5ZC2LG374"
-xcrun notarytool submit dist/GuernikaModelConverter.pkg --keychain-profile "com.guiyec.notarytool" --wait
-xcrun stapler staple dist/GuernikaModelConverter.pkg
+xcrun notarytool submit dist/GuernikaModelConverter.dmg --keychain-profile "com.guiyec.notarytool" --wait
+xcrun stapler staple dist/GuernikaModelConverter.dmg
